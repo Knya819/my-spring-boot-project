9cd76b487f438baa839d0e7ea5203ad28500dc1c
Kelly Nya <knya819@gmail.com>
2024-08-13 17:29:26 +0100

    Created new class Staff

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,10 +1,13 @@
 package com.example.demo;
 
+import com.example.demo.staff.Staff;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDate;
+import java.time.Month;
 import java.util.List;
 
 @SpringBootApplication
@@ -14,8 +17,16 @@ public class DemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
-	@GetMapping// In other to be served as a restful endpoint we need to annotate it
-	public List<String> hello(){    //method, returns a json array
-		return List.of("Hello", "World");
+	@GetMapping    // In order to be served as a restful endpoint we need to annotate it
+	public List<Staff> hello(){    //method, returns a json array
+		return List.of(
+				new Staff(
+						1L,
+						"Kelly",
+						"kelly@gmail.com",
+						LocalDate.of(2000, Month.JANUARY,1),
+						24
+				)
+		);
 	}
 }
