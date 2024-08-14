0e24ce730a0f8ed92a356af20eb0f36a97a21056
ilia_lymonov <ilialimits222@gmail.com>
2024-08-14 12:50:14 -0400

    introduction to lombok, repository

--- src/main/java/com/example/demo/staff/Staff.java
+++ src/main/java/com/example/demo/staff/Staff.java
@@ -1,12 +1,17 @@
 package com.example.demo.staff;
 
 import jakarta.persistence.*;
+import lombok.*;
 
 import java.time.LocalDate;
 
 @Entity // for Hibernate
-@Table
 
+// Consider using Lombok to reduce boilerplate code!
+
+@Data // Generates getters and setters for all fields, equals(), hashCode(), and toString() methods.
+@AllArgsConstructor // Generates a constructor with one parameter for each field in the class.
+@NoArgsConstructor // Generates a no-argument constructor for the class.
 public class Staff {
     @Id
     @SequenceGenerator(name = "staff_sequence",
@@ -23,81 +28,5 @@ public class Staff {
     private LocalDate DOB;
     private Integer age;
 
-
-    public Staff() {
-    }
-
-    public Staff(long id,
-                 String name,
-                 String email,
-                 LocalDate DOB,
-                 Integer age) {
-        this.id = id;
-        this.name = name;
-        this.email = email;
-        this.DOB = DOB;
-        this.age = age;
-    }
-
-    public Staff(String name,
-                 String email,
-                 LocalDate DOB,
-                 Integer age) {
-        this.name = name;
-        this.email = email;
-        this.DOB = DOB;
-        this.age = age;
-    }
-
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public LocalDate getDOB() {
-        return DOB;
-    }
-
-    public void setDOB(LocalDate DOB) {
-        this.DOB = DOB;
-    }
-
-    public Integer getAge() {
-        return age;
-    }
-
-    public void setAge(Integer age) {
-        this.age = age;
-    }
-
-    @Override
-    public String toString() {
-        return "Staff{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", email='" + email + '\'' +
-                ", DOB=" + DOB +
-                ", age=" + age +
-                '}';
-    }
 
 }
--- src/main/java/com/example/demo/staff/StaffController.java
+++ src/main/java/com/example/demo/staff/StaffController.java
@@ -1,6 +1,7 @@
 package com.example.demo.staff;
 
 
+import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,19 +12,24 @@ import java.util.List;
 
 @RestController
 @RequestMapping(path = "api/v1/staff")
+@AllArgsConstructor // Use AllArgsConstructor annotation to inject dependencies easier!
 public class StaffController {
 
+    //No need of Autowired because of @AllArgsConstructor
     private final StaffService staffService;
+    private final StaffRepository staffRepository;
 
-    @Autowired //we are say that the StaffService should be autowired for us
-    public StaffController (StaffService staffService){
-        this.staffService = staffService;
-    }
 
-    @GetMapping
+    @GetMapping// Consider writing an endpoint ex. @GetMapping("/staff")
     public List<Staff> getStaff(){
         return staffService.getStaff();
     }
+
+    @GetMapping("/all")// Consider writing an endpoint ex. @GetMapping(/staff)
+    public List<Staff> getAllStaff(){
+        return staffRepository.findAll();
+    }
+
 
 
     // @GetMapping    // In order to be served as a restful endpoint we need to annotate it
